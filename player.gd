3bdb8a0fcabab8aaff13b32e0d2bd89123753138
Sarah Schulte <git@sarah.engineer>
2023-03-11 20:46:39 -0800

    network test

---
+++ player.gd
@@ -0,0 +1,22 @@
+extends Sprite2D
+
+
+var pid = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2()
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	translate(velocity * delta * 300)
