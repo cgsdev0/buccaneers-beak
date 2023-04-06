301f6459eb81017aa310c946775ba7665fdb267f
Sarah Schulte <git@sarah.engineer>
2023-04-06 13:19:47 -0700

    fixes

--- UI/inventory_hints.gd
+++ UI/inventory_hints.gd
@@ -10,12 +10,12 @@ func _ready():
 func on_pickup(item: GameState.Pickup):
 	var hint = InventoryHint.new()
 	hint.text = "+1 " + GameState.item_names[item]
+	hint.modulate = Color.RED
 	add_child(hint)
 	
 func on_map(i: int):
 	var hint = InventoryHint.new()
 	hint.text = "+1 Map Piece"
-	hint.modulate = Color.DARK_RED
 	add_child(hint)
 
 func on_delete(item: GameState.Pickup, count: int):
